97c77794f36b2d518459bd079c27479a290a7370
elmyrockers <elmyrockers@gmail.com>
2026-03-15 22:15:34 +0800

    Update main() and add dump() for debugging

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
-
 import (
 	_ "github.com/joho/godotenv/autoload"
+
+	"encoding/json"
+	"fmt"
 )
+
+// For debugging
+func dump(data interface{}) {
+	b, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		fmt.Println("Error printing data:", err)
+		return
+	}
+	fmt.Println(string(b))
+}
+
 
 
 
 func main(){
-	stat := stats{}
-	langs := stat.most_used_languages( 10 )
+	langs := newStats().most_used_languages( 10 )
 	dump( langs )
 }
